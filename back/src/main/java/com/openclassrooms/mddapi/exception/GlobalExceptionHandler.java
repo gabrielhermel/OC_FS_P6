7c79379e644c986ac7ddea30a513c0ea02fc8d8d
Gabriel Hermel <gabrieljonathanhermel@gmail.com>
2026-02-20 22:35:21 +0100

    removed debug code

--- back/src/main/java/com/openclassrooms/mddapi/exception/GlobalExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/mddapi/exception/GlobalExceptionHandler.java
@@ -143,8 +143,6 @@ public class GlobalExceptionHandler {
    */
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
-    ex.printStackTrace();
-
     ErrorResponse response = new ErrorResponse(
         HttpStatus.INTERNAL_SERVER_ERROR.value(),
         "Une erreur inattendue s'est produite",
